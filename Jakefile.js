e39c5e118523b81f2161baa3823b5776eb47d8dd
Scott Taylor <scott@railsnewbie.com>
2011-08-03 03:03:51 -0400

    first "real" commit - get something going

---
+++ Jakefile.js
@@ -0,0 +1,82 @@
+var async = require('async');
+var childProcess = require("child_process");
+
+desc("Lint + Run tests");
+task("default", ["lint", "test"]);
+
+var backtick = function(command, args, options, callback) {
+  var stream = childProcess.spawn(command, args, options);
+  var stdoutData = "";
+  var stderrData = "";
+
+  stream.stdout.on('data', function(data) {
+    if (callback) {
+      stdoutData += data;
+    } else {
+      process.stdout.write(data);
+    }
+  });
+
+  stream.stderr.on('data', function(data) {
+    if (callback) {
+      stderrData += data;
+    } else {
+      process.stderr.write(data);
+    }
+  });
+
+  if (callback) {
+    stream.on("exit", function() {
+      callback(stderrData, stdoutData);
+    });
+  }
+};
+
+desc("Run tests");
+task("test", [], function() {
+  process.env.NODE_ENV = "test";
+  backtick("vows", [], null);
+});
+
+desc("Run tests");
+task("spec", ["test"]);
+
+var EXCLUDED_LINT_FILES = [
+  /node_modules/,
+];
+
+desc("Run js lint (jsl)");
+task("lint", [], function() {
+  backtick("find", [__dirname, '-name', '*.js'], null, function(err, out) {
+    if (err) { throw new Error(err); }
+    var files = out.split("\n").filter(function(f) {
+      if (!f) { return false; }
+      return EXCLUDED_LINT_FILES.every(function(rule) {
+        return !f.match(rule);
+      });
+    });
+    async.forEach(files, function(file, callback) {
+      var args = [file];
+      if (/pub\/js/.test(file)) {
+        args.push("--browser");
+      }
+      backtick("jslint", args, null, function(err, out) {
+        if (err) {
+          callback(err);
+        }
+        else if (out && !/No errors found/.test(out)) {
+          console.error(out);
+          callback(null);
+        }
+      });
+    }, function(err) {
+      if (err && /execvp\(\)/.test(err.toString())) {
+        throw new Error("jslint not installed or in path, " +
+          "install via `npm install jslint -g`");
+      }
+      else if (err) {
+        throw new Error(err);
+      }
+    });
+  });
+});
---
+++ spec/simple-spec.js
@@ -0,0 +1,40 @@
+var vows = require("vows");
+var assert = require("assert");
+var loop = require(__dirname + "/../lib/loop");
+
+vows.describe("Simple source to source transformation").addBatch({
+  'variables': {
+    "it should use define in place of var": function(str) {
+      var self = this;
+
+      loop.transform("(define x)", function(str) {
+        console.log(str);
+
+        assert.equal(str, "var x;");
+      });
+    },
+
+    "it should use the correct variable in define": function(str) {
+      var self = this;
+
+      loop.transform("(define foo)", function(str) {
+        console.log(str);
+
+        assert.equal(str, "var foo;");
+      });
+    }
+  }
+
+  // "it should transform an assignment": function() {
+  //   assert.equal(loop.transform("(= x 10)"), "x = 10;\n");
+  // },
+  //
+  // "it should transform two assignments to only use one var statment": "pending",
+  //
+  // "it should transform a lambda expression": function() {
+  //   var str = "function(x) { return x; }"
+  //   assert.equal(transformer.transform("(lambda (x) x)"), str);
+  // },
+  //
+  // "it should "
+}).export(module);
