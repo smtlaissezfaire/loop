d2f6d8d8e231fba50156d0df2541b460ddba3e1e
Scott Taylor <scott@railsnewbie.com>
2011-12-04 19:56:57 -0500

    whitespace

--- spec/integration/keywords-spec.js
+++ spec/integration/keywords-spec.js
@@ -89,5 +89,4 @@ vows.describe("phase 3: transform from eval'ed syntax into uglifyjs parse tree (
     var out = loop.toUglifyTree([evaled]);
     assert.equal(JSON.stringify(out), JSON.stringify(uglifyTree));
   }
-
 }).export(module);
