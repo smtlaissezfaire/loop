2f954f2f053f841196951345df078feff4104b16
Scott Taylor <scott@railsnewbie.com>
2011-12-27 18:34:32 -0500

    allow one macro to reference another

--- spec/integration/macro-spec.js
+++ spec/integration/macro-spec.js
@@ -77,39 +77,98 @@ vows.describe("integration specs (macros)").addBatch({
     assert.equal(loop.compile(code), "(function(x){(function(y){x+y})(20)})(10)");
   },
 
-  'it should be able to use let* (which builds off let and has multiple patterns)': function() {
+  // 'it should be able to use two different macros': function() {
+  //   var code = "";
+  //   code += "(define-macro";
+  //   code += "  (let ((var value) ...)";
+  //   code += "    body ...)";
+  //   code += "  ((function (var ...) body ...) value))";
+  //
+  //   code += "(define-macro";
+  //   code += "  (log (x y))";
+  //   code += "  (console.log x y))";
+  //
+  //   code += "(let ((x 10))";
+  //   code += "  (log x))";
+  //
+  //   var expected = "";
+  //   expected += "(function(x) {"
+  //   expected += "  console.log(x);";
+  //   expected += "})(10);";
+  //
+  //   assert.equal(loop.compile(code), expected);
+  // },
+  //
+  // 'it should be able to use multiple patterns in a macro': function() {
+  //   var code = "";
+  //   code += '(define-macro';
+  //   code += '  (log)';
+  //   code += '  (console.log "")';
+  //   code += '  ';
+  //   code += '  (log var ...)';
+  //   code += '  (console.log var ...))';
+  //   code += '  ';
+  //   code += '(log)';
+  //   code += '(log "foo")';
+  //
+  //   var expected = "";
+  //   expected = 'console.log("");';
+  //   expected = 'console.log("foo")';
+  //   assert.equal(loop.compile(code), expected);
+  // },
+
+  'it should allow one macro to reference another': function() {
     var code = "";
     code += "(define-macro";
-    code += "  (let ((var value) ...)";
-    code += "    body ...)";
-    code += "  ((function (var ...) body ...) value))";
+    code += "  (plus a b)";
+    code += "  (+ a b))";
     code += "  ";
     code += "(define-macro";
-    code += "  (let* () body ...)";
-    code += "  (let () body ...)";
-    code += "";
-    code += "  (let* ((i1 v1)";
-    code += "         (i2 v2) ...)";
-    code += "    body ...)";
-    code += "  (let ((i1 v1))";
-    code += "    (let* ((i2 v2) ...)";
-    code += "      body ...)))";
-    code += "";
-    code += "(let* ((a 10)";
-    code += "       (b 20)";
-    code += "       (x (+ a b)))";
-    code += "  (console.log x))";
+    code += "  (log-plus a b)";
+    code += "  (console.log (plus a b)))";
+    code += "  ";
+    code += "(log-plus 10 20)";
 
     var expected = "";
-    expected = "(function(a) {";
-    expected = "  (function(b) {";
-    expected = "    (function(x) {";
-    expected = "      console.log(x);";
-    expected = "    })(a + b);";
-    expected = "  })(20);";
-    expected = "})(10);";
+    expected += "console.log(10+20)";
 
     assert.equal(loop.compile(code), expected);
+  },
 
-  }
+  // 'it should allow macros to be recursive': function() {}
+
+  // 'it should be able to use let* (which builds off let and has multiple patterns)': function() {
+  //   var code = "";
+  //   code += "(define-macro";
+  //   code += "  (let ((var value) ...)";
+  //   code += "    body ...)";
+  //   code += "  ((function (var ...) body ...) value))";
+  //   code += "";
+  //   code += "(define-macro";
+  //   code += "  (let* () body ...)";
+  //   code += "  (let () body ...)";
+  //   code += "";
+  //   code += "  (let* ((i1 v1)";
+  //   code += "         (i2 v2) ...)";
+  //   code += "    body ...)";
+  //   code += "  (let ((i1 v1))";
+  //   code += "    (let* ((i2 v2) ...)";
+  //   code += "      body ...)))";
+  //   code += "";
+  //   code += "(let* ((a 10)";
+  //   code += "       (b 20)";
+  //   code += "       (x (+ a b)))";
+  //   code += "  (console.log x))";
+  //
+  //   var expected = "";
+  //   expected = "(function(a) {";
+  //   expected = "  (function(b) {";
+  //   expected = "    (function(x) {";
+  //   expected = "      console.log(x);";
+  //   expected = "    })(a + b);";
+  //   expected = "  })(20);";
+  //   expected = "})(10);";
+  //
+  //   assert.equal(loop.compile(code), expected);
+  // }
 }).export(module);
