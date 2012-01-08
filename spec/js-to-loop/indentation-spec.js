425cdae0834b231eb584561b13349b5bb6153347
Scott Taylor <scott@railsnewbie.com>
2012-01-07 17:30:53 -0800

    add a pending spec

--- spec/js-to-loop/indentation-spec.js
+++ spec/js-to-loop/indentation-spec.js
@@ -3,11 +3,27 @@ var assert = require("assert");
 var loop = require(__dirname + "/../../lib/loop");
 
 vows.describe("reverse compiler - indentation").addBatch({
-  'it put a multi expression var statement on multiple lines at the same indentation)': function() {
+  'it should put a multi expression var statement on multiple lines at the same indentation)': function() {
     var source = "var x = 10, y = 20;";
     var expected = '';
     expected += '(var (x 10)\n';
     expected += '     (y 20))';
     assert.equal(loop.reverseCompile(source), expected);
   },
+
+  'it should indent an lambda assignment': function() {} //: function() {
+  //   var source = '';
+  //   source += 'var x = function() {';
+  //   source += '  x + y;';
+  //   source += '  z + y;';
+  //   source += '};';
+  //
+  //   var expected = '';
+  //   expected += '(define x\n';
+  //   expected += '  (lambda ()\n';
+  //   expected += '    (+ x y)\n';
+  //   expected += '    (+ z y)))';
+  //
+  //   assert.equal(loop.reverseCompile(source), expected);
+  // }
 }).export(module);
