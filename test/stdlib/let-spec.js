94df1b3eea001cf7b0907fed5a8ab50804f99794
Scott Taylor <scott@railsnewbie.com>
2013-02-24 20:01:23 -0800

    add let as a built-in

---
+++ test/stdlib/let-spec.js
@@ -0,0 +1,31 @@
+var assert = require("assert");
+var loop = require(__dirname + "/../../lib/loop");
+
+describe("built in let", function() {
+  it('should be able to use the let with 2 args', function() {
+    var code = '';
+    code += '(let ((x 10)';
+    code += '      (y 20))';
+    code += '  (console.log (+ x y)))';
+
+    var expected = '';
+    expected += '(function(x,y){';
+    expected += 'console.log(x+y)';
+    expected += '})(10,20)';
+
+    assert.equal(loop.compile(code), expected);
+  });
+
+  it('should be able to use the let with zero args', function() {
+    var code = '';
+    code += '(let ()';
+    code += '  (console.log (+ x y)))';
+
+    var expected = '';
+    expected += '(function(){';
+    expected += 'console.log(x+y)';
+    expected += '})()';
+
+    assert.equal(loop.compile(code), expected);
+  });
+});
